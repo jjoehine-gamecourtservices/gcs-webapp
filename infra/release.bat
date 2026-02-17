d710878f02efa3ea079f6b7270178eabaefe0437
Joe Hine <joe@gamecourtservices.com>
2026-02-17 08:05:58 -0600

    infra: harden prod compose, enforce version pinning, add release compose, tighten ignore rules

--- infra/release.bat
+++ infra/release.bat
@@ -24,17 +24,17 @@ if "%VERSION%"=="" (
 )
 
 echo Building images (local)...
-docker compose -p gcs_build -f docker-compose.yml -f docker-compose.dev.yml build
+docker compose -p gcs_build -f docker-compose.yml -f docker-compose.release.yml build
 if errorlevel 1 exit /b 1
 
-echo Tagging images as gcs/api:%VERSION% and gcs/web:%VERSION% ...
-docker image tag gcs/api:local gcs/api:%VERSION%
+echo Tagging images as gamecourt/gcs-api:%VERSION% and gamecourt/gcs-web:%VERSION% ...
+docker image tag gamecourt/gcs-api:local gamecourt/gcs-api:%VERSION%
 if errorlevel 1 exit /b 1
-docker image tag gcs/web:local gcs/web:%VERSION%
+docker image tag gamecourt/gcs-web:local gamecourt/gcs-web:%VERSION%
 if errorlevel 1 exit /b 1
 
 echo Done.
 echo Tagged:
-echo   gcs/api:%VERSION%
-echo   gcs/web:%VERSION%
+echo   gamecourt/gcs-api:%VERSION%
+echo   gamecourt/gcs-web:%VERSION%
 exit /b 0
